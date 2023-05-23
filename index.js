f735ed39130b3283c02eb024ede21327e5082211
sahil <sahilshityalkar05@gmail.com>
2023-05-23 23:09:40 +0530

    add referance code in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array
+// let myLibrary = [];
+
+// function Book() {
+//   // the constructor...
+// }
+
+// function addBookToLibrary() {
+//   // do stuff here
+// }
